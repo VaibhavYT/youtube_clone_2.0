0d04e0d38b48648943fa04f121557878200e1b79
Vaibhav Jaiswal <vaibhav.jaiswal.vj91@gmail.com>
2023-07-28 17:01:49 +0530

    routing fixed

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -21,7 +21,7 @@ const Feed = () => {
         <Sidebar selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />
         
         <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: "#fff", }}>
-          Copyright © 
+          Copyright © 2023
         </Typography>
       </Box>
 
